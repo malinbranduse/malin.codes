af23a492bb2a2ed8d331d145256b4e810a4a6136
Mălin Brândușe <malin.branduse@gmail.com>
2017-07-15 16:40:14 +0300

    revamp style + fonts

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-    <div itemscope itemtype="http://schema.org/WebPage" class="main-content">
+    <div itemscope itemtype="http://schema.org/WebPage">
 
-        <div class="main-content__container">
+        <section class="main-content">
+            <div class="main-content__container">
 
-            <div v-html="logo" class="logo"></div>
+                <div v-html="logo" class="logo"></div>
 
-            <div class="wrapper bg">
+                <div class="wrapper bg">
 
-                <h1>Hello there</h1>
+                    <h1>Hello there</h1>
 
-                <p itemprop="author" itemscope itemtype="http://schema.org/Person">
-                    I'm <span itemprop="name">Mălin Brândușe</span>, a {{getAge('1997/11/13')}}yr old
+                    <p itemprop="author" itemscope itemtype="http://schema.org/Person">
+                        I'm <span itemprop="name">Mălin Brândușe</span>, a {{getAge('1997/11/13')}}yr old
                     <span itemprop="jobTitle">Web Developer</span> from <span itemprop="workLocation">Romania</span>
-                </p>
+                    </p>
 
-                <ul class="socials">
-                    <li v-for="social in socials" class="social">
-                        <a target="_blank" :href="social.url" v-html="social.svg" :class="social.name"></a>
-                    </li>
-                </ul>
+                    <ul class="socials">
+                        <li v-for="social in socials" class="social">
+                            <a target="_blank" :href="social.url" v-html="social.svg" :class="social.name"></a>
+                        </li>
+                    </ul>
 
+                </div>
             </div>
-        </div>
+        </section>
+
+        <section>
+
+        </section>
+
     </div>
 </template>
 
@@ -54,16 +61,16 @@
                 axios.get(`/src/assets/svg/${this.socials[i].name}.svg`)
                     .then(svg => {
                         this.socials[i].svg = svg.data;
-                        console.log(this.socials[i]);
                         this.$forceUpdate();
                     })
                     .catch(e => console.log(e));
 
-            axios.get('/src/assets/svg/logo.svg')
+            axios.get('/src/assets/svg/logo-color.svg')
                 .then(svg => {
                     this.logo = svg.data;
                 })
                 .catch(e => console.log(e));
+            this.$forceUpdate();
         },
         methods: {
             getAge(dateString) {
@@ -80,141 +87,97 @@
     }
 </script>
 
-<style lang="scss">
-    body {
-        margin: 0;
-    }
+<style lang="sass">
+    @import './sass/mixins'
 
-    $color: #0d355a;
+    html, body
+        margin: 0
+        font-family: 'Fira Sans', sans-serif;
 
-    .main-content {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: $color;
-        display: block;
-        height: 100vh;
-        font-size: 0;
-        background: #2BC0E4;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #EAECC6, #2BC0E4);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to right, #EAECC6, #2BC0E4); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-        background-size: 400% 400%;
+    $color: #424242
+    $color-links: #0d355a
 
-        -webkit-animation: gradientAnimation 9s ease infinite;
-        -moz-animation: gradientAnimation 9s ease infinite;
-        -o-animation: gradientAnimation 9s ease infinite;
-        animation: gradientAnimation 9s ease infinite;
+    .main-content
+        -webkit-font-smoothing: antialiased
+        -moz-osx-font-smoothing: grayscale
+        text-align: center
+        color: $color
+        display: block
+        height: 100vh
+        font-size: 0
 
-        @-webkit-keyframes gradientAnimation {
-            0%{background-position:0% 82%}
-            50%{background-position:100% 19%}
-            100%{background-position:0% 82%}
-        }
-        @-moz-keyframes gradientAnimation {
-            0%{background-position:0% 82%}
-            50%{background-position:100% 19%}
-            100%{background-position:0% 82%}
-        }
-        @-o-keyframes gradientAnimation {
-            0%{background-position:0% 82%}
-            50%{background-position:100% 19%}
-            100%{background-position:0% 82%}
-        }
-        @keyframes gradientAnimation {
-            0%{background-position:0% 82%}
-            50%{background-position:100% 19%}
-            100%{background-position:0% 82%}
-        }
+        +main-bg(#51e4d6, #2ce8fb)
 
-        &:before {
-            content: '';
-            display: inline-block;
-            vertical-align: middle;
-            height: 100%;
-        }
-        &__container {
-            display: inline-block;
-            vertical-align: middle;
-            text-align: left;
-            font-size: 20px;
-            width: 100%;
-            max-width: 600px;
+        &:before
+            content: ''
+            display: inline-block
+            vertical-align: middle
+            height: 100%
 
-            @media (max-width: 600px) {
-                font-size: 14px;
-            }
-        }
-    }
+        &__container
+            display: inline-block
+            vertical-align: middle
+            text-align: left
+            font-size: 20px
+            width: 100%
+            max-width: 600px
 
-    .bg {
-        background: rgba(#fff, 0.4);
-    }
+            @media (max-width: 600px)
+                font-size: 14px
 
-    .wrapper {
-        padding: 20px 30px;
-    }
+    .bg
+        //background: rgba(#fff, 0.4)
+        border: 4px solid rgba(#fff, 0.4)
 
-    h1 {
-        font-size: 60px;
-        line-height: 1;
-        font-weight: 300;
-        margin-bottom: 0;
 
-        @media (max-width: 600px) {
-            font-size: 42px;
-        }
-    }
+    .wrapper
+        padding: 20px 30px
 
-    p {
-        font-size: 18px;
-        font-weight: 500;
+    h1
+        font-family: 'Lora', serif
+        font-size: 60px
+        line-height: 1
+        font-weight: 300
+        margin-bottom: 0
 
-        span {
-            font-weight: 900;
-        }
-    }
+        @media (max-width: 600px)
+            font-size: 42px
 
-    .socials {
-        list-style-type: none;
-        padding: 0;
-        margin-top: 40px;
-    }
+    p
+        font-size: 18px
+        font-weight: 500
+        span
+            font-weight: 900
 
-    .social {
-        display: inline-block;
-        text-align: center;
-        margin: 0 10px;
+    .socials
+        list-style-type: none
+        padding: 0
+        margin-top: 40px
 
-        a {
-            display: block;
-            transition: .2s ease;
+    .social
+        display: inline-block
+        text-align: center
+        margin: 0 10px
+        a
+            display: block
+            transition: .2s ease
+            &:hover
+                transform: scale(1.1)
+                svg
+                    fill: lighten($color-links, 10%)
+        svg
+            width: 30px
+            height: 30px
+            fill: lighten($color, 10%)
 
-            &:hover {
-                transform: scale(1.1);
-            }
-        }
-        svg {
-            width: 30px;
-            height: 30px;
-            fill: lighten($color, 10%);
-        }
-    }
+    .logo svg
+        width: 100px
+        height: 100px
+        padding: 10px 30px
 
-    .logo svg {
-        width: 100px;
-        height: 100px;
-        padding: 10px 30px;
-
-        path {
-            fill: #0d355a;
-        }
-
-        @media (max-width: 600px) {
-            width: 80px;
-            height: 80px;
-        }
-    }
+        @media (max-width: 600px)
+            width: 80px
+            height: 80px
 
 </style>
