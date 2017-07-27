dee846552777f6700ce6c83a3555913a9a100f67
Mălin Brândușe <malin.branduse@gmail.com>
2017-07-27 22:06:34 +0300

    fix loader animation

--- src/App.vue
+++ src/App.vue
@@ -133,7 +133,7 @@
                 if (loader !== null) {
                     loader.className = 'hidden';
                     'webkitAnimationEnd mozAnimationEnd oAnimationEnd oanimationend animationend'.split(' ')
-                        .map(name => loader.addEventListener(name, () => loader.parentNode.removeChild(loader), false));
+                        //.map(name => loader.addEventListener(name, () => loader.parentNode.removeChild(loader), false));
                 }
             },
             getAge(dateString) {
@@ -191,7 +191,7 @@
     $color-links: #0d355a
 
     @import './sass/mixins'
-    //@import './sass/loader'
+    @import './sass/loader'
     @import './sass/header'
 
     html, body, *
