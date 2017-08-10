c097746ed6488ae8c93dfdd6f4ead7a00d9ca3c4
Mălin Brândușe <malin.branduse@gmail.com>
2017-08-11 01:02:07 +0300

    fixing js files, jquery + removing author from posts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,15 @@
 import '../sass/main.sass';
-import './canvas';
+import $ from 'jquery';
+import canvas from './canvas';
+import post from './post';
+
+$(document).ready( () => {
+
+	if ($('body').hasClass('home-template')) {
+		console.log('home');
+		canvas();
+	}
+
+	if($('body').hasClass('post-template'))
+		post();
+});
